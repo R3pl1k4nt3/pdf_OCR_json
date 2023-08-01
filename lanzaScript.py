63f09bc992ba613b362fd2e71ccafb70f6866b05
agomezo <agomezo@guardiacivil.es>
2023-08-01 12:27:56 +0200

    solved scripts bash/py. Cuenta resultados anidados

--- lanzaScript.py
+++ lanzaScript.py
@@ -1,30 +1,61 @@
 import tkinter as tk
 from tkinter import messagebox  # Import the messagebox module
 import subprocess
+import os
 
 def ejecutar_script():
     texto_buscar_1 = entry_texto_buscar_1.get()
     texto_buscar_2 = entry_texto_buscar_2.get()
-    if texto_buscar_1 and texto_buscar_2:
+
+    if texto_buscar_1:
+        # Construir el comando para ejecutar el script bash
+        comando_bash = f'buscarEnPdf "{texto_buscar_1}"'
+        if texto_buscar_2:
+            comando_bash += f' "{texto_buscar_2}"'
+
         # Ejecutar el script bash con los textos ingresados como argumentos
-        comando_bash = f'buscarEnPdf "{texto_buscar_1}" "{texto_buscar_2}"'
-        subprocess.run(comando_bash, shell=True)
+        resultado = subprocess.run(comando_bash, shell=True, capture_output=True, text=True)
+
+        # Mostrar mensaje con la salida del comando grep
+        mensaje = f"Se ha ejecutado el comando grep con los valores ingresados.\n"
+        mensaje += resultado.stdout.strip()  # Agregar la salida del comando grep al mensaje
+
+        # Obtener el nombre del archivo CSV generado
+        nombre_archivo = obtener_nombre_archivo(texto_buscar_1, texto_buscar_2)
+
         # Contar las líneas del archivo CSV
-        archivo_csv = f"resultados{texto_buscar_1}_{texto_buscar_2}.csv"
-        lineas_csv = contar_lineas_csv(archivo_csv)
+        lineas_csv = contar_lineas_csv(nombre_archivo)
+
         # Mostrar mensaje con el resultado
         if lineas_csv == 0:
-            mensaje = "No hay coincidencias."
+            mensaje += "No hay coincidencias."
         else:
-            mensaje = f"Existen coincidencias. El archivo contiene {lineas_csv} línea(s)."
+            mensaje += f"Existen coincidencias. El archivo contiene {lineas_csv} línea(s)."
         messagebox.showinfo("Ejecución completada", mensaje)
+
     else:
-        messagebox.showerror("Error", "Por favor, ingresa ambos valores a buscar.")
+        messagebox.showerror("Error", "Por favor, ingresa al menos el primer valor a buscar.")
+
+def obtener_nombre_archivo(texto_buscar_1, texto_buscar_2):
+    carpeta_resultados = "/home/alex/resultados"
+    if texto_buscar_2:
+        return f"resultados{texto_buscar_1}_{texto_buscar_2}.csv"
+    else:
+        # Buscar el último archivo que coincida con el primer valor en la carpeta "resultados"
+        archivos_en_carpeta = os.listdir(carpeta_resultados)
+        archivos_coincidentes = [archivo for archivo in archivos_en_carpeta if archivo.startswith(f"resultados{texto_buscar_1}")]
+        archivos_coincidentes.sort(reverse=True)  # Ordenar para obtener el más reciente primero
+        if archivos_coincidentes:
+            return archivos_coincidentes[0]
+        else:
+            return f"resultados{texto_buscar_1}.csv"
 
 def contar_lineas_csv(archivo_csv):
+    carpeta_resultados = "/home/alex/resultados"
     try:
-        with open(archivo_csv, 'r') as archivo:
-            return sum(1 for linea in archivo)
+        with open(os.path.join(carpeta_resultados, archivo_csv), 'r') as archivo:
+            lineas = archivo.readlines()
+            return len(lineas)
     except FileNotFoundError:
         return 0
 
@@ -48,8 +79,8 @@ label_texto_buscar_1.pack()
 entry_texto_buscar_1 = tk.Entry(ventana)
 entry_texto_buscar_1.pack()
 
-# Etiqueta y campo de entrada para el segundo texto a buscar
-label_texto_buscar_2 = tk.Label(ventana, text="Ingrese el segundo texto a buscar:")
+# Etiqueta y campo de entrada para el segundo texto a buscar (opcional)
+label_texto_buscar_2 = tk.Label(ventana, text="Ingrese el segundo texto a buscar (opcional):")
 label_texto_buscar_2.pack()
 
 entry_texto_buscar_2 = tk.Entry(ventana)
