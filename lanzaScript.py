473fd61f7c027d543ab640cc1cbacddb6c34395d
agomezo <agomezo@guardiacivil.es>
2023-08-02 07:44:12 +0200

    funcional

--- lanzaScript.py
+++ lanzaScript.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox  # Import the messagebox module
+from tkinter import Scrollbar, Text, messagebox  # Import the Text widget and Scrollbar module
 import subprocess
 import os
 
@@ -16,48 +16,77 @@ def ejecutar_script():
         # Ejecutar el script bash con los textos ingresados como argumentos
         resultado = subprocess.run(comando_bash, shell=True, capture_output=True, text=True)
 
-        # Mostrar mensaje con la salida del comando grep
+        # Almacenar el resultado del primer grep
+        resultado_grep_1 = resultado.stdout
+
+        # Mostrar mensaje con el resultado del contador CSV
         mensaje = f"Se ha ejecutado el comando grep con los valores ingresados.\n"
-        mensaje += resultado.stdout.strip()  # Agregar la salida del comando grep al mensaje
+        mensaje += f"\nContador CSV: {obtener_lineas_csv(texto_buscar_1, texto_buscar_2, resultado_grep_1)}"
 
-        # Obtener el nombre del archivo CSV generado
-        nombre_archivo = obtener_nombre_archivo(texto_buscar_1, texto_buscar_2)
-
-        # Contar las líneas del archivo CSV
-        lineas_csv = contar_lineas_csv(nombre_archivo)
-
-        # Mostrar mensaje con el resultado
-        if lineas_csv == 0:
-            mensaje += "No hay coincidencias."
+        # Mostrar los resultados del comando grep en la interfaz
+        if texto_buscar_2:
+            resultado_grep_2 = subprocess.run(f'grep -i "{texto_buscar_2}"', input=resultado_grep_1, shell=True, capture_output=True, text=True)
+            mensaje += f"\n\nResultados del segundo grep:\n{resultado_grep_2.stdout.strip()}"
         else:
-            mensaje += f"Existen coincidencias. El archivo contiene {lineas_csv} línea(s)."
-        messagebox.showinfo("Ejecución completada", mensaje)
+            mensaje += f"\n\nResultados del primer grep:\n{resultado_grep_1.strip()}"
+        
+        mostrar_resultados(mensaje)
 
     else:
         messagebox.showerror("Error", "Por favor, ingresa al menos el primer valor a buscar.")
+
+def obtener_lineas_csv(texto_buscar_1, texto_buscar_2, resultado_grep_1):
+    nombre_archivo = obtener_nombre_archivo(texto_buscar_1, texto_buscar_2)
+    lineas_csv = contar_lineas_csv(nombre_archivo, resultado_grep_1)
+    if lineas_csv == 0:
+        return "No hay coincidencias."
+    else:
+        return f"Existen coincidencias. El archivo contiene {lineas_csv} línea(s)."
 
 def obtener_nombre_archivo(texto_buscar_1, texto_buscar_2):
     carpeta_resultados = "/home/alex/resultados"
     if texto_buscar_2:
         return f"resultados{texto_buscar_1}_{texto_buscar_2}.csv"
     else:
-        # Buscar el último archivo que coincida con el primer valor en la carpeta "resultados"
         archivos_en_carpeta = os.listdir(carpeta_resultados)
         archivos_coincidentes = [archivo for archivo in archivos_en_carpeta if archivo.startswith(f"resultados{texto_buscar_1}")]
-        archivos_coincidentes.sort(reverse=True)  # Ordenar para obtener el más reciente primero
+        archivos_coincidentes.sort(reverse=True)
         if archivos_coincidentes:
             return archivos_coincidentes[0]
         else:
             return f"resultados{texto_buscar_1}.csv"
 
-def contar_lineas_csv(archivo_csv):
+def contar_lineas_csv(archivo_csv, resultado_grep_1):
     carpeta_resultados = "/home/alex/resultados"
     try:
         with open(os.path.join(carpeta_resultados, archivo_csv), 'r') as archivo:
             lineas = archivo.readlines()
-            return len(lineas)
+            lineas_csv = len(lineas)
+
+            # Si el resultado del primer grep es el mismo archivo CSV, restar 1 al contador de líneas
+            if archivo_csv in resultado_grep_1:
+                lineas_csv -= 1
+
+            return lineas_csv
     except FileNotFoundError:
         return 0
+
+def mostrar_resultados(mensaje):
+    # Crear una nueva ventana para mostrar los resultados
+    ventana_resultados = tk.Toplevel(ventana)
+    ventana_resultados.title("Resultados de búsqueda")
+
+    # Cuadro de texto para mostrar los resultados
+    cuadro_texto_resultados = Text(ventana_resultados, wrap=tk.WORD)
+    cuadro_texto_resultados.pack(fill=tk.BOTH, expand=True)
+
+    # Barra de desplazamiento para el cuadro de texto
+    scrollbar = Scrollbar(ventana_resultados, command=cuadro_texto_resultados.yview)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    cuadro_texto_resultados.config(yscrollcommand=scrollbar.set)
+
+    # Insertar los resultados en el cuadro de texto
+    cuadro_texto_resultados.insert(tk.END, mensaje)
 
 # Crear la ventana principal
 ventana = tk.Tk()
